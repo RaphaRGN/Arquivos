4c4b063b0b5ffd659e8b8d792b607aa77f15b5ad
Raphael <raphaelgoettzinger@gmail.com>
2024-03-14 21:57:00 -0300

    Commit JS

---
+++ Site 2 - Rede Social/ScriptMEUTI.js
@@ -0,0 +1,10 @@
+var numerolinhas = 14;
+var corpotabela = document.getElementById("tabela-corpo")
+
+for(var i = 0; i < numerolinhas; i++){
+
+var novalinha = document.createElement("tr");
+newRow.innerHTML = '<td contenteditable="true"></td><td contenteditable="true"></td>';
+tbody.appendChild(newRow);
+
+}
