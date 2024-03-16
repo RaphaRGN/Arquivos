75594dc53d24ab623a926577ae7fddc14982a15c
Raphael <raphaelgoettzinger@gmail.com>
2024-03-16 12:46:24 -0300

    Commit de pequenos ajustes

--- Site 2 - Rede Social/ScriptMEUTI.js
+++ Site 2 - Rede Social/ScriptMEUTI.js
@@ -1,8 +1,19 @@
-    const btnEditar = document.getElementById('btnEditar');
-    const camposEditaveis = document.querySelectorAll('#tabela tbody td[contenteditable="false"]');
+document.addEventListener('DOMContentLoaded', function () {
+  const btnEditar = document.getElementById('btnEditar');
+  const btnSalvar = document.getElementById('btnSalvar');
+  const camposEditaveis = document.querySelectorAll('#tabela tbody td[contenteditable="false"]');
 
-    btnEditar.addEventListener('click', function() {
-      camposEditaveis.forEach(function(campo) {
-        campo.contentEditable = 'true';
+  btnEditar.addEventListener('click', function () {
+      camposEditaveis.forEach(function (campo) {
+          campo.contentEditable = 'true';
+          campo.style.backgroundColor = '#f8f8f8'; // Adiciona um indicador visual para células editáveis
       });
-    })
+  });
+
+  btnSalvar.addEventListener('click', function () {
+      camposEditaveis.forEach(function (campo) {
+          campo.contentEditable = 'false';
+          campo.style.backgroundColor = ''; // Remove o indicador visual para células editáveis
+      });
+  });
+});
