309a472f4b65a370569154a68d042d01c65f3921
Raphael <raphaelgoettzinger@gmail.com>
2024-03-17 23:58:05 -0300

    Inicio da OS's manutenção

--- Site 2 - Rede Social/ScriptMEUTI.js
+++ Site 2 - Rede Social/ScriptMEUTI.js
@@ -6,14 +6,14 @@ document.addEventListener('DOMContentLoaded', function () {
   btnEditar.addEventListener('click', function () {
       camposEditaveis.forEach(function (campo) {
           campo.contentEditable = 'true';
-          campo.style.backgroundColor = '#f8f8f8'; // Adiciona um indicador visual para células editáveis
+          campo.style.backgroundColor = '#f8f8f8';
       });
   });
 
   btnSalvar.addEventListener('click', function () {
       camposEditaveis.forEach(function (campo) {
           campo.contentEditable = 'false';
-          campo.style.backgroundColor = ''; // Remove o indicador visual para células editáveis
+          campo.style.backgroundColor = ''; 
       });
   });
 });
